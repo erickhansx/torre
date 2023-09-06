07b7b37e36ed8bb5349d17f161a3ae8318d9524a
erickhansx <erickhansx@gmail.com>
2023-09-05 19:31:20 -0600

    Fixed active colors for navtabs component

--- src/ui/NavTabs.jsx
+++ src/ui/NavTabs.jsx
@@ -1,17 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavTabs = () => {
+  const location = useLocation();
+
+  const isSelected = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <div className="flex w-full items-end justify-center text-text-color-d2">
       <Link
         to={'/'}
-        className="bg-primary py-4 px-14 text-center w-[25%] border-t-2 border-text-color-d2/30 rounded-t-xl hover:bg-stone-900 active:bg-slate-800 transition-all duration-300"
+        className={` py-4 px-14 text-center w-[25%] border-t-2 border-text-color-d2/30 rounded-t-xl hover:bg-stone-900 active:bg-slate-800 transition-all duration-300 ${
+          isSelected('/') ? 'bg-primary' : ''
+        }`}
       >
         Search
       </Link>
       <Link
         to={'/favorites'}
-        className="bg-primary py-4 px-14 text-center w-[25%] border-t-2 border-text-color-d2/30 rounded-t-xl hover:bg-stone-900 active:bg-slate-800 transition-all duration-300"
+        className={` py-4 px-14 text-center w-[25%] border-t-2 border-text-color-d2/30 rounded-t-xl hover:bg-stone-900 active:bg-slate-800 transition-all duration-300 ${
+          isSelected('/favorites') ? 'bg-primary' : ''
+        }`}
       >
         Favorites
       </Link>
